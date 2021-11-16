a29c2a7da94e4e9c53660ef409f9f0a25c05d9e3
Evan <ehyde@intrinio.com>
2021-11-16 15:40:47 -0700

    update to readme and sample app

--- realtime.js
+++ realtime.js
@@ -1,20 +1,55 @@
 "use strict"
 const Client = require("./index")
 const accessKey = "OjEyOTk1MzNkMjEwZmY4MmYxNTRiMzhhYmY3NWIwOGYy"
-const provider = "REALTIME"
-const symbols = ["GOOG"]
-const config = {
-    tradesOnly: true,
-    symbols: symbols
-}
+
+let trades = new Map()
+let quotes = new Map()
+let maxTradeCount = 0
+let maxCountTrade = null
+let maxQuoteCount = 0
+let maxCountQuote = null
 
 function onTrade(trade) {
-    console.log(trade)
+    let key = trade.Symbol
+    if (trades.has(key)) {
+        let value = trades.get(key)
+        if (value + 1 > maxTradeCount) {
+            trades.set(key, value + 1)
+            maxTradeCount = value + 1
+            maxCountTrade = trade
+        } 
+    }
+    else trades.set(key, 1)
 }
 
 function onQuote(quote) {
-    console.log(quote)
+    let key = quote.Symbol + ":" + quote.Type
+    if (quotes.has(key)) {
+        let value = quotes.get(key)
+        if (value + 1 > maxQuoteCount) {
+            quotes.set(key, value + 1)
+            maxQuoteCount = value + 1
+            maxCountQuote = quote
+        }
+    }
+    else quotes.set(key, 1)
 }
 
-let client = new Client(accessKey, provider, onTrade, onQuote, config)
-client.join()
+let client = new Client(accessKey, onTrade, onQuote)
+client.join("GOOG", true)
+
+setInterval(() => {
+    if (maxTradeCount > 0) {
+        console.log("Most active security (by trade frequency): %s (%i updates)", maxCountTrade, maxTradeCount)
+    }
+    if (maxQuoteCount > 0) {
+        console.log("Most active security (by quote frequency): %s (%i updates)", maxCountQuote, maxQuoteCount)
+    }
+    let totalMsgCount = client.getTotalMsgCount()
+    if (totalMsgCount > 0) {
+        console.log("Total updates received = %i", totalMsgCount)
+    }
+    else {
+        console.log("No updates")
+    }
+}, 10000)
