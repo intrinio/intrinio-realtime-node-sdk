29baddfc45750a016837a018527e6c659e0bb983
ssnyder <ssnyder@intrinio.com>
2024-03-13 20:44:19 -0500

    isDarkpool

--- index.js
+++ index.js
@@ -417,6 +417,8 @@ class IntrinioRealtime {
   _parseTrade(bytes) {
     let symbolLength = bytes[2];
     let conditionLength = bytes[26 + symbolLength];
+    let marketCenter = readUnicodeString(bytes, 4 + symbolLength, 6 + symbolLength);
+    let isDarkpool = (!marketCenter) || marketCenter == "\0" || marketCenter == "D" || marketCenter == "E" || marketCenter.trim().length === 0;
     return {
       Type: this._getMessageType(bytes[0]),
       Symbol: readString(bytes, 3, 3 + symbolLength),
@@ -425,8 +427,9 @@ class IntrinioRealtime {
       Timestamp: readUInt64(bytes, 14 + symbolLength),
       TotalVolume: readUInt32(bytes, 22 + symbolLength),
       SubProvider: this._getSubProvider(bytes[3 + symbolLength]),
-      MarketCenter: readUnicodeString(bytes, 4 + symbolLength, 6 + symbolLength),
-      Condition: conditionLength > 0 ? readString(bytes, 27 + symbolLength, 27 + symbolLength + conditionLength) : ""
+      MarketCenter: marketCenter,
+      Condition: conditionLength > 0 ? readString(bytes, 27 + symbolLength, 27 + symbolLength + conditionLength) : "",
+      IsDarkpool: isDarkpool
     }
   }
 
