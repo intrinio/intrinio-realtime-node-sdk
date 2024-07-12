c97be10fead9b20c3c6c50b26ea01089e29263e3
Shawn Snyder <ssnyder@intrinio.com>
2024-07-12 11:30:59 -0500

    deprioritize web page usage, add client headers back in

--- index.js
+++ index.js
@@ -615,7 +615,10 @@ class IntrinioRealtime {
         try {
           console.info("Intrinio Realtime Client - Websocket initializing (public key)");
           let wsUrl = this._getWebSocketUrl();
-          this._websocket = new WebSocket(wsUrl);
+          this._websocket = new WebSocket(wsUrl, {}, {headers: {
+              [CLIENT_INFO_HEADER_KEY]: CLIENT_INFO_HEADER_VALUE,
+              [MESSAGE_VERSION_HEADER_KEY]: MESSAGE_VERSION_HEADER_VALUE
+            }});
           this._websocket.binaryType = "arraybuffer";
           this._websocket.onopen = () => {
             console.log("Intrinio Realtime Client - Websocket connected (public key)");
